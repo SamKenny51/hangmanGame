8eaa63c3b3ea9247aab2729839c0e02d00b07640
ThaeliosRaedkin <sgkenny@ncsu.edu>
2020-05-22 20:21:08 -0400

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,60 +1,67 @@
+""" DOCSTRING: A Simple Text-based Hangman Game """
+
 import string
-from bs4 import BeautifulSoup
-import requests
 import random
 import re
 import math
 from collections import Counter
+from bs4 import BeautifulSoup
+import requests
 
-def fetchWord(word_length):
+def fetch_word(word_length):
     """
     takes int argument: word_length
     returns str: word, dict: word_counter, str: definition
     """
-    letters = list(string.ascii_lowercase)
-    vowels = ['a','e','i','o','u']
-    letters = [i + v for i in letters for v in vowels]
+    _letters = list(string.ascii_lowercase)
+    _vowels = ['a', 'e', 'i', 'o', 'u']
+    _letters = [i + v for i in _letters for v in _vowels]
 
     # Create a word from consonant+vowel pairs
-    word = ''
-    for i in range(word_length):
-        word += random.choice(letters)
-    
-    url = f'https://www.dictionary.com/misspelling?term={word}&s=t'
-    response = requests.get(url)
-        
-    soup_word = BeautifulSoup(response.text, 'lxml')
-    word = soup_word.find('h2', class_='spell-suggestions-subtitle css-6gthty e19m0k9k0').find('a').text
-    word_counter = Counter(word)
-    
-    def_url = f'https://www.dictionary.com/browse/{word}'
-    def_response = requests.get(def_url)
-    soup_def = BeautifulSoup(def_response.text, 'lxml')
-    definition = soup_def.find('span', class_='one-click-content css-1p89gle e1q3nk1v4').text
+    _word = ''.join([random.choice(_letters) for _ in range(word_length)])
 
-    return word, word_counter, definition
+    _url = f'https://www.dictionary.com/misspelling?term={_word}&s=t'
+    _response = requests.get(_url)
 
-def blurDefinition(word, definition):
+    _soup_word = BeautifulSoup(_response.text, 'lxml')
+    _sug_head = _soup_word.find('h2', class_='spell-suggestions-subtitle css-6gthty e19m0k9k0')
+    _word = _sug_head.find('a').text
+    _word_counter = Counter(_word)
+
+    _def_url = f'https://www.dictionary.com/browse/{_word}'
+    _def_response = requests.get(_def_url)
+    _soup_def = BeautifulSoup(_def_response.text, 'lxml')
+    _definition = _soup_def.find('span', class_='one-click-content css-1p89gle e1q3nk1v4').text
+
+    return _word, _word_counter, _definition
+
+def blur_definition(_word, _definition):
     """ regular expression to censor spoiler words """
-    new_def = re.sub(word,len(word)*"*", definition)
-    if word[-1] == 'd': new_def = re.sub(word[:-1],(len(word)-1)*"*", new_def)
-    if word[-1] == 'y': new_def = re.sub(word[:-1],(len(word)-1)*"*", new_def)
-    if word[-2:] == 'ly': new_def = re.sub(word[:-2],(len(word)-2)*"*", new_def)
-    if word[-2:] == 'er': new_def = re.sub(word[:-1],(len(word)-2)*"*", new_def)
-    return new_def
+    _new_def = re.sub(word, len(word)*"*", definition)
+    if _word[-1] == 'd':
+        _new_def = re.sub(_word[:-1], (len(_word)-1)*"*", _new_def)
+    if _word[-1] == 'y':
+        _new_def = re.sub(_word[:-1], (len(_word)-1)*"*", _new_def)
+    if _word[-2:] == 'ly':
+        _new_def = re.sub(_word[:-2], (len(_word)-2)*"*", _new_def)
+    if _word[-2:] == 'er':
+        _new_def = re.sub(_word[:-1], (len(_word)-2)*"*", _new_def)
+    return _new_def
 
-def createHiddenString(word, char_set):
-    """ create string that is germane to the specified character set so that the users don't get confused """
-    new_string = ''
-    for c in word:
-        if c in char_set:
-            new_string += '_ '
+def create_hidden_string(_word, _char_set):
+    """ Create a censored version of word """
+    _new_string = ''
+    for _c in _word:
+        if _c in _char_set:
+            _new_string += '_ '
         else:
-            new_string += c + ' '
-    return new_string[:-1]
+            _new_string += _c + ' '
+    return _new_string[:-1]
 
-class Picture(object):
-
+class Picture:
+    '''
+    A simple class for making text user interfaces
+    '''
     def __init__(self, xSize=50, ySize=50, style='#'):
         '''
         Picture Class constructor
@@ -68,9 +75,9 @@ class Picture(object):
             self.ySize = 2
         else:
             self.ySize = ySize
-            
+
         self.picture = [xSize * style + '\n']
-        for i in range(ySize-2):
+        for _ in range(ySize-2):
             self.picture.append(style + ' ' * (xSize-2) + style  + '\n')
         self.picture.append(xSize * style)
         self.top_margin = 2
@@ -82,35 +89,33 @@ class Picture(object):
         String representation of Picture Object
         Printed to screen when print( Picture ) is called
         '''
-        rstring = ''
-        for line in self.picture:
-            rstring += line
-        return rstring
+        return ''.join(self.picture)
 
-    def addText(self, text, xPos, yPos, justify="Left", wrap=False):
+    def add_text(self, text, xPos, yPos):
         '''
         Add text to picture at (xPos, yPos)
-        TODO: include right and center justify options
+        TODO: include right and center justify options and wordwrap functionality
         '''
-        words = text.split(' ')
+        _words = text.split(' ')
         xPos_0 = xPos
-        for word in words:
-            if xPos + len(word) > self.xSize - self.right_margin:
+        for _word in _words:
+            if xPos + len(_word) > self.xSize - self.right_margin:
                 xPos = xPos_0
                 yPos += 1
                 if yPos == self.ySize - 2:
                     break
             if yPos <= len(self.picture):
-                self.picture[yPos] = self.picture[yPos][:xPos] + word + " " + self.picture[yPos][(xPos+len(word)+1):]
-                xPos += len(word) + 1
-        
-    def addLine(self, from_xPos, from_yPos, to_xPos, to_yPos, style):
+                self.picture[yPos] = (self.picture[yPos][:xPos] + _word + " " +
+                                      self.picture[yPos][(xPos+len(_word)+1):])
+                xPos += len(_word) + 1
+
+    def add_line(self, from_xPos, from_yPos, to_xPos, to_yPos, style='#'):
         '''
-        Add a line to picture starting at (from_xPos, from_yPos) to (to_xPos,to_yPos) with the style character 
-        '''            
+        Add a line to picture starting at from
+        '''
         if from_xPos == to_xPos:
-            for yPos in range(from_yPos,to_yPos+1):
-                self.addDot(from_xPos, yPos, '#')
+            for yPos in range(from_yPos, to_yPos+1):
+                self.add_dot(from_xPos, yPos, style)
         else:
             dx = to_xPos - from_xPos
             dy = to_yPos - from_yPos
@@ -119,63 +124,56 @@ class Picture(object):
             xPos, yPos = from_xPos, from_yPos
             while round(xPos) != to_xPos:
                 #print(xPos, yPos)
-                self.addDot(round(xPos), round(yPos), '#')
+                self.add_dot(round(xPos), round(yPos), style)
                 xPos += xStep
                 yPos += yStep
-            self.addDot(round(xPos), round(yPos), '#')
+            self.add_dot(round(xPos), round(yPos), style)
 
-    def addDot(self, xPos, yPos, style='#'):
+    def add_dot(self, xPos, yPos, style='#'):
         '''
-        Add a dot at (xPos, yPos) with the style character
+        Add a single dot based on style to the Picture interface at (xPos,yPos)
         '''
         self.picture[yPos] = self.picture[yPos][:xPos] + style + self.picture[yPos][xPos+1:]
 
 if __name__ == '__main__':
     char_set = [chr(i+97) for i in range(26)]
-    
-    pict = Picture(50,25)
-    pict.addText("H A N G M A N", 3, 2)
-    pict.addLine(0,16,49,16,'#')
-    pict.addLine(25,0,25,16,'#')
-    pict.addText("Incorrect Guesses", 28, 2)
+    pict = Picture(50, 25)
+    pict.add_text("H A N G M A N", 3, 2)
+    pict.add_line(0, 16, 49, 16, '#')
+    pict.add_line(25, 0, 25, 16, '#')
+    pict.add_text("Incorrect Guesses", 28, 2)
 
     while True:
-        
-        word, word_counter, definition = fetchWord(4)
-        blurred_def = blurDefinition(word, definition.lower())
-        if word[-1] == 'd':
-            blurred_def = blurDefinition(word, blurred_def)
-        if word[-1] == 'y':
-            blurred_def = blurDefinition(word, blurred_def)
-        if word[-2:] == 'ly':
-            blurred_def = blurDefinition(word, blurred_def)
-        hidden_word = createHiddenString(word, char_set)
-        pict.addText(hidden_word, 3, 4)
-        pict.addText("Hint: "+blurred_def,3,18)
+        word, word_counter, definition = fetch_word(4)
+        blurred_def = blur_definition(word, definition.lower())
+        hidden_word = create_hidden_string(word, char_set)
+        pict.add_text(hidden_word, 3, 4)
+        pict.add_text("Hint: "+blurred_def, 3, 18)
         print(pict)
-        
-        nGuesses = 10
-        incorrect = ''
-        while nGuesses >= 1:
+
+        NGUESSES = 10
+        INCORRECT = ''
+        while NGUESSES >= 1:
             player_letter = input("Pick a letter (a-z)! ")
             if len(player_letter) == 0 or player_letter not in char_set:
                 continue
-            elif word_counter[player_letter[0].lower()] > 0:
+
+            if word_counter[player_letter[0].lower()] > 0:
                 word_counter[player_letter] = 0
                 i = 0
                 for c in word:
                     if player_letter == c:
                         hidden_word = hidden_word[:i] + player_letter + hidden_word[(i+1):]
                     i += 2
-                pict.addText(hidden_word, 3, 4)
+                pict.add_text(hidden_word, 3, 4)
             else:
                 print("Wrong!")
-                incorrect += player_letter + " "
-                pict.addText(incorrect, 28, 4)
-                nGuesses -= 1
-                
+                INCORRECT += player_letter + " "
+                pict.add_text(INCORRECT, 28, 4)
+                NGUESSES -= 1
+
             print(pict)
-            if nGuesses == 0:
+            if NGUESSES == 0:
                 print(f"Game Over! The word was {word}!")
                 break
 
@@ -186,9 +184,9 @@ if __name__ == '__main__':
         playAgain = input("Would you like to play again? (y/n) ")
         if playAgain != 'y':
             break
-        
-        pict.addText(len(hidden_word)*" ", 3, 4)
-        pict.addText("Hint: "+len(blurred_def)*" ", 3,18)
-        pict.addText(len(blurred_def)*" ", 3,19)
-        pict.addText(len(blurred_def)*" ", 3,20)
-        pict.addText(len(incorrect)*" ", 28,4)
+
+        pict.add_text(len(hidden_word)*" ", 3, 4)
+        pict.add_text("Hint: "+len(blurred_def)*" ", 3, 18)
+        pict.add_text(len(blurred_def)*" ", 3, 19)
+        pict.add_text(len(blurred_def)*" ", 3, 20)
+        pict.add_text(len(INCORRECT)*" ", 28, 4)
